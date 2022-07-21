3bcc649c9a3172f4026d9901afe97e3ec776c2b2
ozBenTik <108788482+ozBenTik@users.noreply.github.com>
2022-07-21 22:00:18 +0300

    started the details ui design

--- ui-movies/src/main/java/details/DetailsFragment.kt
+++ ui-movies/src/main/java/details/DetailsFragment.kt
@@ -3,16 +3,24 @@ package details
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.example.ui_movies.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.core.TmdbImageManager
+import com.example.moviestmdb.core_ui.util.SpaceItemDecoration
 import com.example.ui_movies.databinding.FragmentDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 import extensions.launchAndRepeatWithViewLifecycle
-import kotlinx.coroutines.flow.collect
 import timber.log.Timber
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
@@ -20,27 +28,115 @@ class DetailsFragment : Fragment() {
     lateinit var binding: FragmentDetailsBinding
     private val viewModel: DetailsViewModel by viewModels()
 
+    private lateinit var recommendedAdapter: RecommendedMoviesCarrouselAdapter
+    private lateinit var actorsAdapter: ActorsCarrouselAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    @Inject
+    lateinit var tmdbImageManager: TmdbImageManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initRecommendedAdapter()
+        initActorsAdapter()
+
         launchAndRepeatWithViewLifecycle {
-            viewModel.state.collect{
-                Timber.i("$$$$ recs = ${it.recommendations}  actors = ${it.actors}")
+            viewModel.state.collect{ uiState ->
+                Timber.i("$$$$ recs = ${uiState.recommendations}  actors = ${uiState.actors}")
+
+                uiState.movieDetails?.let { movie ->
+                    binding.title.text = movie.title
+                    binding.detailsOverviewTextView.text = movie.overView
+                    binding.detailsRatingPrecentageTextView.text = "${movie.popularityPrecentage}%"
+                    binding.detailsNumberOfVotesTextView.text = "${(movie.voteCount ?: 0)/1000}K votes"
+                    binding.detailsStatusTextView.text =
+                        if (LocalDate.parse(movie.releaseDate).isAfter(LocalDate.now()))
+                            "To be released at\n${movie.releaseDate}"
+                        else
+                            "released"
+
+                    movie.posterPath?.let { posterPath ->
+                        Glide.with(binding.root)
+                            .load(
+                                tmdbImageManager.getLatestImageProvider().getPosterUrl(
+                                    path = posterPath,
+                                    imageWidth = binding.detailsPosterImageView.width
+                                )
+                            )
+                            .into(binding.detailsPosterImageView)
+
+                    }
+
+                    movie.backdropPath?.let { backdropPath ->
+                        Glide.with(binding.root)
+                            .load(
+                                tmdbImageManager.getLatestImageProvider().getBackdropUrl(
+                                    path = backdropPath,
+                                    imageWidth = binding.root.width
+                                )
+                            )
+                            .into(binding.detailsBackdropImageView)
+
+                    }
+                }
+
+                binding.actorsView.setLoading(uiState.actorsRefreshing)
+                actorsAdapter.submitList(uiState.actors)
+
+                binding.recommendedMoviesView.setLoading(uiState.recommendationsRefreshing)
+                recommendedAdapter.submitList(uiState.recommendations)
+
             }
         }
+    }
+
+    private fun initRecommendedAdapter() {
+        recommendedAdapter = RecommendedMoviesCarrouselAdapter(
+            tmdbImageManager.getLatestImageProvider()
+        )
+        binding.recommendedMoviesView.recyclerView.run {
+            adapter = recommendedAdapter
+            layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
+            val itemSpacing =
+                resources.getDimension(com.example.core_ui.R.dimen.spacing_normal).toInt()
+            addItemDecoration(
+                SpaceItemDecoration(
+                    itemSpacing, -itemSpacing
+                )
+            )
+        }
+
+        binding.recommendedMoviesView.title.text = "Recommended Movies"
+    }
+
+    private fun initActorsAdapter() {
+        actorsAdapter = ActorsCarrouselAdapter(
+            tmdbImageManager.getLatestImageProvider()
+        )
+
+        binding.actorsView.recyclerView.run {
+            adapter = actorsAdapter
+            layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+
+            val itemSpacing =
+                resources.getDimension(com.example.core_ui.R.dimen.spacing_normal).toInt()
+            addItemDecoration(
+                SpaceItemDecoration(
+                    itemSpacing, -itemSpacing
+                )
+            )
+        }
+
+        binding.actorsView.title.text = "Top Billed Cast"
     }
 
 }
